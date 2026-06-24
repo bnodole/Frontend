5d1d2fb8eb654c3724692c381a19853570fe1f43
bnodole <bnodole01@gmail.com>
2026-06-24 16:24:27 +0545

    HAHAHA

--- ReactOld/Ecommerce/src/App.tsx
+++ ReactOld/Ecommerce/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { Navbar } from './assets/components/Navbar/Navbar'
 import Content1 from './assets/components/Contents/Content1'
 import Features from './assets/components/Features/Features'
